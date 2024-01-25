9524e9c2c0af9f54f5a511c2ea7ad89991cfd21a
Ajay-Patel009 <ajay.patel@appinventiv.com>
2024-01-25 17:15:02 +0530

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'; // 1.1 Import the mongoose module
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module'; // 2.1 Import the product module
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://ajaypatel:appinventiv@cluster0.1zmesdm.mongodb.net/product'), // 1.2 Setup the database
+    ProductModule, 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
